fc7ab3dbb6976b60236f6fd161e1ae71193ef4bd
Nimesh-Nagar <n.nimesh02@gmail.com>
2023-07-26 11:23:04 +0530

    updates

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,17 +2,13 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
-
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
@@ -32,9 +28,7 @@ def generate_launch_description():
             parameters=[twist_mux_params, {'use_sim_time': True}],
             remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
         )
-
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
-
 
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -49,8 +43,6 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
-
-
 
     # Launch them all!
     return LaunchDescription([
